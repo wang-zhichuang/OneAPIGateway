d323682336369916fbe12a920800bde65b27b7f3
OneSeek <1668207735@qq.com>
2020-03-31 01:28:21 +0800

    Netty官网例子

--- src/main/java/cn/oneseek/core/nettyserver/DiscardServerHandler.java
+++ src/main/java/cn/oneseek/core/nettyserver/DiscardServerHandler.java
@@ -8,16 +8,18 @@ import io.netty.buffer.ByteBuf;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
  */
 public class DiscardServerHandler extends ChannelHandlerAdapter { // (1)
 
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
     }
+
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
