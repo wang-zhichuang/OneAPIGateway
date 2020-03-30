4778b5df3ba32098f001792c6a9a3ac8a1772b8a
OneSeek <1668207735@qq.com>
2020-03-31 00:03:34 +0800

    删除NIO，尝试用Netty实现

---
+++ src/main/java/cn/oneseek/service/Data.java
@@ -0,0 +1,14 @@
+package cn.oneseek.service;
+
+/**
+ * @Author: Chuang
+ * @Date: 2020/3/30 21:06
+ */
+public class Data {
+    static boolean isRun = true;
+
+    public void setRun(boolean run) {
+        isRun = run;
+    }
+
+}
